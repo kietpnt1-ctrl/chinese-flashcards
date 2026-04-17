4f8ad290defdbf81355a42866b56771bbbacf7ea
kietpnt1-ctrl <kietpnt1@gmail.com>
2026-04-17 12:05:28 +0700

    Convert application into a Progressive Web App (PWA) with offline-support logic

---
+++ sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'chinese-flashcards-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json',
+  './icon.png',
+  './hamster.jpg',
+  './penguin.jpg',
+  './lizard.jpg',
+  './beaver.jpg',
+  './hsk_words.txt'
+];
+
+// Install Event: Initialize Cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event: Cleanup old caches if version changes
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event: Cache First Strategy
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version if found
+        if (response) {
+          return response;
+        }
+        // Otherwise fetch from network
+        return fetch(event.request);
+      })
+  );
+});
